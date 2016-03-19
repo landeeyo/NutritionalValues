bf0635d59da00747778ecf6c42474e1172e56adb
landeeyo <landeeyo@10g.pl>
2016-03-19 19:11:00 +0100

    Added first part of data to data service. Extended locales, improved grams filter and details page.

--- scripts/filters/grams.js
+++ scripts/filters/grams.js
@@ -1,5 +1,8 @@
 app.filter('grams', function() {
     return function(input) {
+        if(input == undefined || input == 0){
+            return "-";
+        }
         return (!!input) ? input + " g" : '';
     }
 });
